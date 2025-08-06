76c243c2676587286c8ce2c32ea70a8499facc31
anajulia-oliveyra <anajulia.oliveyra@gmail.com>
2025-08-06 18:26:40 -0300

    Adicionei editarCliente

--- main.cpp
+++ main.cpp
@@ -59,7 +59,82 @@ void listarCliente(){
     }
 }
 
-void 
+void editarCliente(){
+    string nomeCliente;
+    cout<<"Digite o nome do cliente que voce quer editar:"<<endl;
+    cin.ignore();
+    getline(cin,nomeCliente);
+
+    ifstream arquivoAntigo("clientes_cadastrados.txt");
+
+    if(arquivoAntigo.is_open()){
+        string linha;
+        string arquivoTemporario = "temp.txt";
+        ofstream arquivoNovo(arquivoTemporario,ios::app);
+
+        if(arquivoNovo.is_open()){
+            bool clienteEncontrado=false;
+
+            while(getline(arquivoAntigo,linha)){
+                size_t posicao = linha.find(nomeCliente);
+
+                if(posicao!=string::npos){
+                    clienteEncontrado=true;
+
+                    cout<<"Cliente encontrado: "<<linha<< endl;
+
+                    Cliente clienteEditado;
+                    size_t pos1=linha.find(",");
+                    size_t pos2=linha.find(",",pos1+1);
+                    size_t pos3=linha.find(",",pos2+1);
+
+                    clienteEditado.nome=linha.substr(0,pos1);
+                    clienteEditado.cpf=linha.substr(pos1+1,pos2-pos1-1);
+                    clienteEditado.email=linha.substr(pos2+1,pos3-pos2-1);
+                    clienteEditado.idade=stoi(linha.substr(pos3+1));
+
+                    int opcao=0;
+                    do{
+                        cout << "\nO que deseja editar?\n";
+                        cout << "1. Nome\n";
+                        cout << "2. CPF\n";
+                        cout << "3. E-mail\n";
+                        cout << "4. Idade\n";
+                        cout << "0. Finalizar edição\n";
+                        cout << "Escolha: ";
+                        cin >> opcao;
+                        cin.ignore();
+
+                        switch(opcao){
+                            case 1:
+                                cout<<"Novo nome:"<<endl;
+                                getline(cin,clienteEditado.nome);
+                                break;
+                            case 2:
+                                cout<<"Novo cpf: "<<endl;
+                                getline(cin,clienteEditado.cpf);
+                                break;
+                            case 3:
+                                cout<<"Novo email:"<<endl;
+                                getline(cin,clienteEditado.email);
+                                break;
+                            case 4: 
+                                cout<<"Nova idade:" <<endl;
+                                getline(cin,clienteEditado.idade);
+                                break;
+                            case 0:
+                                break;
+                            default:
+                                cout<<"Opção inválida!"<<endl;
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
+}
 
 void excluirCliente(){
     string nomeCliente;
